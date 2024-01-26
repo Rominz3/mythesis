1b66b8be775069d6325d164e72d3ef4e47d591c0
am.janmohammadi <am.janmohammadi@mydigipay.com>
2024-01-26 21:30:43 +0330

    aa

--- code/1.py
+++ code/1.py
@@ -9,4 +9,3 @@ imputer = SimpleImputer(strategy='most_frequent')
 
 df_filled = pd.DataFrame(imputer.fit_transform(df), columns=df.columns)
 df_filled.to_excel(R"C:\Users\ROMINA\Desktop\delete nan\data\2\isfahan(11).xlsx",index=True)
-
